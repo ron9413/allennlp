4cf252372676fd009dc8122d6860fc2b63bb947b
keitakurita <keita.kurita@gmail.com>
2019-03-12 08:59:05 -0400

    NEW: Added a custom trainer with callbacks

--- allennlp/training/__init__.py
+++ allennlp/training/__init__.py
@@ -1,2 +1,4 @@
 from allennlp.training.trainer import Trainer
 from allennlp.training.trainer_base import TrainerBase
+from allennlp.training.trainer_with_callbacks import TrainerWithCallbacks
+from allennlp.training.callbacks import Callback
